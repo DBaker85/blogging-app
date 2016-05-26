a1160752cff6734b7099d69f5e8fcd7eb973fa80
Dbaker85 <badcancer22@gmail.com>
2016-05-26 17:45:12 +0400

    Added angular and angular templates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,68 @@
 
-var expandArea = function(context){
-  var expandBox = '<div class="area"></div>'
-  context.parents('.level').append(expandBox)
-}
+// var expandArea = function(context){
+//   var expandBox = '<div class="area"></div>'
+//   context.parents('.level').append(expandBox)
+// }
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $('.box').on('click', function(){
-    if ($(this).data("expand")==true){
-      $(this)
-        .toggleClass('expand-box')
-        .parents('.level')
-        .toggleClass('expand')
-        .attr('data-theme',$(this).data('theme'));
+//   $('.box').on('click', function(){
+//     if ($(this).data("expand")==true){
+//       $(this)
+//         .toggleClass('expand-box')
+//         .parents('.level')
+//         .toggleClass('expand')
+//         .attr('data-theme',$(this).data('theme'));
+//     }
+//   })
+
+// })
+
+'use strict';
+
+
+
+angular
+  .module('BloggingApp', [])
+  .controller('ContentController', ['$scope', function ($scope) {
+      var vm = this;
+      vm.levels = [
+        {
+          tiles: [
+            {
+              'theme': 'twitter',
+              'title':'Twitter',
+              'icon' : 'twitter',
+              'expand': true
+            },
+             {
+              'theme': 'duolingo',
+              'title':'Duolingo',
+              'icon' : 'duolingo',
+              'expand': true
+            },
+          ]
+        },
+        {
+          tiles: [
+            {}
+          ]
+        },
+      ];
+  }])
+  .directive('contentTiles',[function(){
+    return{
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/tile.html',
     }
-  })
+  }])
+  .directive('postBloc',[function(){
+    return{
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/post.html',
+    }
+  }])
 
-})
+
