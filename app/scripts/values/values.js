da8bc42e27c011cae17fb641cd8de0ee8416cb39
DBaker85 <badcancer22@gmail.com>
2016-07-20 17:37:23 +0200

    Added values and cons file and fixed styling

---
+++ app/scripts/values/values.js
@@ -0,0 +1,3 @@
+angular
+  .module('BloggingApp')
+  .value('articleExpanded', false);
--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ gulp.task('icon',function (callback) {
 });
 
 gulp.task('js', function() {
-  return gulp.src(['./app/scripts/app.js','./app/scripts/filters/*.js','./app/scripts/factories/*.js','./app/scripts/directives/*.js','./app/scripts/controllers/*.js'])
+  return gulp.src(['./app/scripts/app.js','./app/scripts/values/*.js','./app/scripts/filters/*.js','./app/scripts/factories/*.js','./app/scripts/directives/*.js','./app/scripts/controllers/*.js'])
     .pipe(plumber({
         errorHandler: function(err){
           gutil.log(gutil.colors.red(err.name)+' in plugin '+gutil.colors.magenta(err.plugin)+' : '+err.message);
